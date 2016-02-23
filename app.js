f7ab24340ff4887c992fce331e1cfa50cea763e4
Noah <nmedoff123@gmail.com>
2016-02-22 22:09:53 -0800

    got images to display to page

--- app.js
+++ app.js
@@ -10,32 +10,38 @@ function Image(imagePath){
 
 var bag = new Image('img/bag.jpg');
 var banana = new Image('img/banana.jpg');
+var bathroom = new Image('image/bathroom.jpg')
+var bubblegum = new Image('image/bubblegum.jpg')
 var boots = new Image('img/boots.jpg');
 var chair = new Image('img/chair.jpg');
 var cthulhu = new Image('img/cthulhu.jpg');
+var dogDuck = new Image('img/dog-duck.jpg')
 var dragon = new Image('img/dragon.jpg');
 var pen = new Image('img/pen.jpg');
+var petSweep = new Image('img/pet-sweep.jpg');
 var scissors = new Image('img/scissors.jpg');
 var shark = new Image('img/shark.jpg');
 var sweep = new Image('img/sweep.png');
+var taunttaun = new Image('image/taunttaun.jpg')
 var unicorn = new Image('img/unicorn.jpg');
 var usb = new Image('img/usb.gif');
-var waterCan = new Image('img/watercan.jpg');
-var wineGlass = new Image('img/wineglass.jpg');
+var waterCan = new Image('img/water-can.jpg');
+var wineGlass = new Image('img/wine-glass.jpg');
 
 var oneEl = document.getElementById('one');
 var twoEl = document.getElementById('two');
 var threeEl = document.getElementById('three');
 
-var theImage = [bag, banana, boots, chair, cthulhu, dragon, pen, scissors, shark, sweep, unicorn, usb, waterCan, wineGlass]
+var theImage = [bag, banana, bathroom, bubblegum, boots, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, taunttaun, unicorn, usb, waterCan, wineGlass]
 
+// Benton helped me figure out this block
 function randomImgs() {
   var index1 = Math.floor(Math.random() * theImage.length);
-  oneEl.textContent = theImage[index1];
+  oneEl.src = theImage[index1].imagePath;
   var index2 = Math.floor(Math.random() * theImage.length);
-  twoEl.textContent = theImage[index2];
+  twoEl.src = theImage[index2].imagePath;
   var index3 = Math.floor(Math.random() * theImage.length);
-  threeEl.textContent = theImage[index3];
+  threeEl.src = theImage[index3].imagePath;
 }
 
 randomImgs();
